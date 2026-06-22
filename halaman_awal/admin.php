48721da29aca019069ce91425bd43dfb68833e41
mochlintang15-source <mochlintang15@gmail.com>
2026-06-22 14:55:58 +0700

    Delete halaman_awal directory

--- halaman_awal/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if($_SESSION['role'] != 'admin'){
-    die("Bukan admin!");
-}
-echo "HALAMAN ADMIN - Welcome ".$_SESSION['email'];
-?>
--- halaman_awal/user.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if($_SESSION['role'] != 'user'){
-    die("Bukan user!");
-}
-echo "HALAMAN USER - Welcome ".$_SESSION['email'];
-?>
